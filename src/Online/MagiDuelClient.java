a22eb8d441ddb891d6c8f2c9b8ed9815707f442a
BeenTrill <nbenjung@me.com>
2018-03-11 16:28:05 -0400

    Ouch, oof, multi threading makes my head hurt :(

---
+++ src/Online/MagiDuelClient.java
@@ -0,0 +1,7 @@
+package Online;
+import java.net.*;
+import java.io.*;
+
+
+public class MagiDuelClient {
+}
---
+++ src/Online/MagiDuelServer.java
@@ -0,0 +1,18 @@
+package Online;
+
+import java.net.*;
+import java.io.*;
+
+public class MagiDuelServer {
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(4444);
+        }
+        catch (IOException e){
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(1);
+        }
+    }
+
+}
